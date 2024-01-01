5cf9e99144a0d6bc571b8662085ff76102ae19b6
LiliiaB <liliya.benatti@gmail.com>
2024-01-01 21:14:55 +0100

    upd css task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,14 @@
-/* Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
- елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього). */
+/* Порахує й виведе в консоль кількість категорій
+ в ul#categories, тобто елементів li.item.
+Для кожного елемента li.item у списку ul#categories
+ знайде й виведе в консоль текст заголовка
+ елемента (тегу <h2>) і кількість елементів у 
+ категорії (усіх <li>, вкладених у нього). */
 
-const categories = document.querySelectorAll("#categories");
+const categories = document.querySelectorAll(".item");
+let total = 0;
 const result = categories.forEach((category) => {
-  let total = 0;
-  total += category;
-} return total);
+  return (total += category);
+});
 
 console.log(`Number of categories: ${result}`);
