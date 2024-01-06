dc993ec7c881ef7e54ad170d9894005ee0264f81
LiliiaB <liliya.benatti@gmail.com>
2024-01-06 12:59:19 +0100

    upd task 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const myDiv = document.querySelector("#boxes");
+
+const myInput = document.querySelector("input");
+let amount = "";
+
+myInput.addEventListener("input", (event) => {
+  if (event.target.value < 1 || event.target.value > 100) {
+    alert("Your number has to be in the range between 1 and 100");
+    event.target.value = "";
+  } else {
+    amount = event.target.value;
+  }
+});
+
+createBtn.addEventListener("click", () => {
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  const randomColor = getRandomHexColor();
+  const result = [];
+  for (let i = 0; i <= amount; i++) {
+    const randomColor = getRandomHexColor();
+    const str = `<div style ="background-color:${randomColor};width:${
+      30 + i * 10
+    }px;height:${30 + i * 10}px"></div>`;
+    result.push(str);
+  }
+  myDiv.innerHTML = result.join("");
+}
+
+function destroyBoxes() {
+  amount = 0;
+  myDiv.innerHTML = "";
+}
